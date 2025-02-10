58d1793809386f3b763f7c46cf6e07d986f1ff1f
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-02-10 11:56:57 +0100

    done counter component

--- first-ng-app/src/app/home/home.component.ts
+++ first-ng-app/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { GreetingComponent } from '../components/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -13,4 +14,6 @@ export class HomeComponent {
   keyUpHandler(e : KeyboardEvent) {
     console.log(`user pressed the ${e.key} key`);
   }
+
+
 }
