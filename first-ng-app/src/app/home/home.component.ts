067eee3c95b9c6251905787377c556ebbd354d48
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-02-10 11:28:08 +0100

    add event-listener

--- first-ng-app/src/app/home/home.component.ts
+++ first-ng-app/src/app/home/home.component.ts
@@ -9,4 +9,8 @@ import { GreetingComponent } from '../components/greeting/greeting.component';
 })
 export class HomeComponent {
   homeMessage = signal('Hello, world!');
+
+  keyUpHandler() {
+    console.log('user typed somethin gin the input');
+  }
 }
