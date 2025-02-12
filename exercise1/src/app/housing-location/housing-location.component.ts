045736154651719561db81b136848efd5cc91b58
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-02-12 13:57:19 +0100

    angular dev 1 add data to component

--- exercise1/src/app/housing-location/housing-location.component.ts
+++ exercise1/src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from './housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+  <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
   styleUrls: ['./housing-location.component.css'],
 })
-export class HousingLocationComponent {
 
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
